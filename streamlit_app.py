67110996996ab72c85377954dcf9f142588500e0
faisal72 <faisal175@yahoo.com>
2026-01-02 15:55:08 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,8 +31,11 @@ cnx = st.connection("snowflake")
 #session = get_active_session()
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"), col("SEARCH_ON"))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredents_list = st.multiselect('Choose up to 5 ingregdients:', my_dataframe, max_selections=5)
 
